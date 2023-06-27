e5db20fd33e898742d6af3706d1916be084ad524
Guilherme dos Santos <gs92dev@gmail.com>
2023-06-27 13:18:47 -0500

    Improvements on menu for mobile version

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 console.log("Hello world!");
 icon = document.getElementsByClassName("ham-icon")[0];
 menu = document.getElementById("menu");
+close = document.querySelectorAll("close");
 icon.onclick = (e) => {
   e.preventDefault();
   if (menu.classList.contains("menu")) {
     menu.classList.remove("menu");
+   
     console.log("clicado1");
   } else {
     menu.classList.add("menu");
+
   }
   console.log("clicado");
 };
